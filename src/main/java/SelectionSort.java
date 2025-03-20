e93949cb9bbc3265ce2356d943f8217b97e7cb0c
adpathak07 <pathak.ada@northeastern.edu>
2025-03-20 13:03:10 -0400

    commit

--- src/main/java/SelectionSort.java
+++ src/main/java/SelectionSort.java
@@ -1,8 +1,24 @@
 class SelectionSort {
     public static void selectionSort(int[] arr) {
         int n = arr.length;
+       // Selection Sort Implementation
+       for (int i = 0; i < n - 1; i++) {
+           // Assume the min is the first element
+           int minIndex = i;
 
-       // STUDENT TODO
+           // Test against elements after i to find the smallest
+           for (int j = i + 1; j < n; j++) {
+               if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+               }
+           }
+
+           // Swap the found minimum element with the first element
+           int temp = arr[minIndex];
+           arr[minIndex] = arr[i];
+           arr[i] = temp;
+       }
+
     }
 
     public static void main(String[] args) {
